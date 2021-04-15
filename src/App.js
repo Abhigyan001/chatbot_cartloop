c681fca58e410558e7e0ce7e76dd7fcdb004adc1
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2021-04-15 20:37:59 +0530

    Reconfigure App.js to include Messageparser nad ActionProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
+import React from 'react';
+import Chatbot from 'react-chatbot-kit'
 import './App.css';
 
-import Chatbot from 'react-chatbot-kit'
+import ActionProvider from './components/ActionProvider';
+import MessageParser from './components/MessageParser';
+import config from './components/config';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot />
+        <Chatbot config={config} actionProvider={ActionProvider} 	    messageParser={MessageParser} />
       </header>
     </div>
   );
