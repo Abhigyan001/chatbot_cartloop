d63812bc728092f6ca78d68424fba0ed706e5554
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2021-04-15 20:11:23 +0530

    Set up initial files for chatbot

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Chatbot from 'react-chatbot-kit'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Chatbot />
+      </header>
+    </div>
+  );
+}
+
+export default App;
