e67be6e13f42071df9a0a7ea8cf4591ef5180442
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-04-19 11:22:34 +0530

    Implement sending users message to the application

--- client/src/actions/watson.js
+++ client/src/actions/watson.js
@@ -2,7 +2,10 @@
 import {
   INPUT_SUCCESS,
   INPUT_FAIL,
-  
+  SESSION_SUCCESS,
+  SESSION_FAIL,
+  MESSAGE_SUCCESS,
+  MESSAGE_FAIL,
 } from "./types";
 
 //  Import axios
@@ -17,3 +20,26 @@ export const userMessage = (message) => async (dispatch) => {
   }
 };
 
+//  Creates a session - API CALL
+export const createSession = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/watson/session");
+    dispatch({ type: SESSION_SUCCESS, payload: res.data });
+  } catch (err) {
+    dispatch({ type: SESSION_FAIL });
+  }
+};
+
+//  Sends the message to the bot - API CALL
+export const sendMessage = (message) => async (dispatch) => {
+  try {
+    const body = { input: message };
+    const res = await axios.post("/api/watson/message", body);
+    dispatch({
+      type: MESSAGE_SUCCESS,
+      payload: res.data.output.generic[0].text,
+    });
+  } catch (err) {
+    dispatch({ type: MESSAGE_FAIL });
+  }
+};
