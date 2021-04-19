039336740f373d6609b4da2b67b9421bb8e95a99
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-04-19 10:16:50 +0530

    Set up Chat functionality

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const Chat = () => {
+const Chat = () => {
+
+  const [message, setMessage] = useState("");
+  const handleClick = async (e) => {
+    const code = e.keyCode || e.which;
+
+    if (code === 13) {
+      console.log(message);
+      userMessage(message);
+      sendMessage(message);
+      setMessage("");
+    }
+  };
   return (
     <div>
       <h1>Cartloop Chatbox</h1>
+      <input
+        id="chatBox"
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={handleClick}
+        value={message}
+      ></input>
     </div>
   )
 }
+
+
+export default Chat;
