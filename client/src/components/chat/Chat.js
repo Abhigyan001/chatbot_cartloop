126bc149a770a2510008924bd35d3f83f941bdf5
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-04-20 19:21:13 +0530

    Change endpoint in client

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+const ENDPOINT = 'https://cartloop-chatbox.herokuapp.com/';
 
 let socket;
 
